94301d45b0a2fca89a97112bc4411868c8b1aa32
kangsun <rd2046sk@163.com>
2018-12-12 18:46:17 +0800

    add hello world file

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<?php
+namespace Kangsun\Demo;
+class Hello
+{
+        private $name;
+        public function __construct( $name = 'World' )
+        {
+                $this->name = $name;
+        }
+        public function getName()
+        {
+                return $this->name;
+        }
+        public function hello()
+        {
+                return 'Hello '.$this->name.'!';
+        }
+}
